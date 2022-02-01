df409b810172d6520588f5d351f73c8170ea3369
Andrés Galván <62343874+afgalvan@users.noreply.github.com>
2022-02-01 18:01:35 -0500

    Initial commit

---
+++ src/Data/Contracts/IRepository.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+
+namespace Data.Contracts
+{
+    public interface IRepository<TEntity>
+    {
+        public void Add(TEntity entity);
+        public IList<TEntity> GetAll();
+    }
+}
---
+++ src/Presentation/App.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows;
+using Data;
+using Entities;
+using Microsoft.Extensions.DependencyInjection;
+using Presentation.Extensions;
+using Presentation.Windows;
+
+namespace Presentation
+{
+    public partial class App : Application
+    {
+        public IServiceProvider ServiceProvider { get; set; }
+
+        public App()
+        {
+            var services = new ServiceCollection();
+            services.AddDataDependencies();
+            services.AddPresentationDependencies();
+            ServiceProvider = services.BuildServiceProvider();
+        }
+
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+            ServiceProvider.GetRequiredService<MainWindow>().Show();
+        }
+    }
+}
